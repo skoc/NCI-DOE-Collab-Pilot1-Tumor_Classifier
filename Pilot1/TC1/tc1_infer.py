7a5237196881ba18d78d1008831c62008cd6d73a
Soner Koc <sonerkoc92@gmail.com>
2021-03-29 00:28:53 +0300

    argparse inputs off

--- Pilot1/TC1/tc1_infer.py
+++ Pilot1/TC1/tc1_infer.py
@@ -82,26 +82,26 @@ def run(gParameters, trained_model_json, trained_model_h5, train_data, test_data
 
 def main(trained_model_json, trained_model_h5, train_data, test_data, tc1_default_model):
 
-    gParameters = initialize_parameters(default_model = tc1_default_model)
+    gParameters = initialize_parameters()#(default_model = tc1_default_model)
     run(gParameters, trained_model_json, trained_model_h5, train_data, test_data)
 
 if __name__ == '__main__':
 
     # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--trained_model_json', help="Json file of Trained Model", required=True)
-    parser.add_argument('--trained_model_h5', help="Weights of Trained Model", required=True)
-    parser.add_argument('--train_data', help="Train Data from the Platform", required=True)
-    parser.add_argument('--test_data', help="Test Data from the Platform", required=True)
-    parser.add_argument('--tc1_default_model', help="Parameters of the model", required=True)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--trained_model_json', help="Json file of Trained Model", required=True)
+    # parser.add_argument('--trained_model_h5', help="Weights of Trained Model", required=True)
+    # parser.add_argument('--train_data', help="Train Data from the Platform", required=True)
+    # parser.add_argument('--test_data', help="Test Data from the Platform", required=True)
+    # parser.add_argument('--tc1_default_model', help="Parameters of the model", required=True)
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    trained_model_json = args.trained_model_json
-    trained_model_h5 = args.trained_model_h5
-    train_data = args.train_data
-    test_data = args.test_data
-    tc1_default_model = args.tc1_default_model
+    trained_model_json = "/tc1/data/tc1.model.json"#args.trained_model_json
+    trained_model_h5 = "/tc1/data/tc1.model.h5"#args.trained_model_h5
+    train_data = "/tc1/data/type_18_300_train.csv"#args.train_data
+    test_data = "/tc1/data/type_18_300_test.csv"#args.test_data
+    tc1_default_model = "/tc1/Pilot1/TC1/tc1_default_model.txt"#args.tc1_default_model
 
     # Path fix for empty tc1_default_model inputs, when it's copied from the required files section
     tc1_default_model = os.path.dirname(train_data) + os.path.basename(tc1_default_model)
