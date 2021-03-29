419fda84b210b4a38a82242105695ca979575f1c
Soner Koc <sonerkoc92@gmail.com>
2021-03-29 15:18:04 -0400

    load_test_data function for just reading test data

--- Pilot1/TC1/tc1.py
+++ Pilot1/TC1/tc1.py
@@ -73,3 +73,17 @@ def load_data(params, train_path, test_path):
 
     return candle.load_Xy_data_noheader(train_path, test_path, params['classes'], usecols,
                                         scaling='maxabs',dtype=params['data_type'])
+
+def load_test_data(params, test_path):
+
+    # train_path = candle.fetch_file(params['data_url'] + params['train_data'], 'Pilot1')
+    # test_path = candle.fetch_file(params['data_url'] + params['test_data'], 'Pilot1')
+    
+    if params['feature_subsample'] > 0:
+        usecols = list(range(params['feature_subsample']))
+    else:
+        usecols = None
+
+
+    return candle.load_Xy_test_data_noheader(test_path, params['classes'], usecols,
+                                        scaling='maxabs',dtype=params['data_type'])
