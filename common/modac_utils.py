abca9f03d8e0e2122259d1bdb7c33f950450305a
Soner Koc <sonerkoc92@gmail.com>
2021-03-29 14:31:12 +0300

    userna and pass for auth modac

--- common/modac_utils.py
+++ common/modac_utils.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 import os
 
 
-modac_user = None
-modac_pass = None
+modac_user = "soner.koc@sbgenomics.com"
+modac_pass = "v.m252627CE"#None
 
 def get_file_from_modac(fname, origin):
     """ Downloads a file from the "Model and Data Clearning House" (MoDAC)
