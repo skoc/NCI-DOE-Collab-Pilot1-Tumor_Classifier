c153a015744cc611662a496c78668a1a50a8b511
Soner Koc <sonerkoc92@gmail.com>
2021-04-02 17:02:43 -0400

    added plot_auc_multi_class

--- Pilot1/TC1/utility.py
+++ Pilot1/TC1/utility.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import accuracy_score, roc_curve, auc, roc_auc_score, \
     precision_recall_fscore_support, matthews_corrcoef, confusion_matrix
+from sklearn.preprocessing import label_binarize
 import sys
 import numpy as np
 import pandas as pd
@@ -8,6 +9,9 @@ import matplotlib.pyplot as plt
 import argparse
 import shutil
 import glob
+
+from scipy import interp
+from itertools import cycle
 
 def eprint(args):
     sys.stderr.write(str(args) + "\n")
@@ -19,10 +23,10 @@ def print_history(history, validation):
     epochs = len(history.history['loss'])
     for epoch in range(epochs):
         if validation:
-            eprint('Epoch {}/{}: accuracy : {:.3f}, loss : {:.3f}, val_accuracy : {:.3f}, val_loss : {:.3f}'.format(epoch + 1, epochs, history.history['accuracy'][epoch], history.history['loss'][epoch], history.history['val_accuracy'][epoch], history.history['val_loss'][epoch]))
+            eprint('Epoch {}/{}: accuracy : {:.3f}, loss : {:.3f}, val_accuracy : {:.3f}, val_loss : {:.3f}'.format(epoch + 1, epochs, history.history['acc'][epoch], history.history['loss'][epoch], history.history['val_acc'][epoch], history.history['val_loss'][epoch]))
         else:
             eprint(
-                'Epoch {}/{}: accuracy : {:.3f}, loss : {:.3f} '.format(epoch + 1, epochs, history.history['accuracy'][epoch], history.history['loss'][epoch]))
+                'Epoch {}/{}: accuracy : {:.3f}, loss : {:.3f} '.format(epoch + 1, epochs, history.history['acc'][epoch], history.history['loss'][epoch]))
 
 # TRAINING OR TESTING
 def plot_auc(y_labels, pred, parameters_dict = {}, title=''):
@@ -41,6 +45,69 @@ def plot_auc(y_labels, pred, parameters_dict = {}, title=''):
     plt.ylabel('Recall')
     plt.xlabel('Fall-out (1-Specificity)')
 
+    conf = ':'.join(map(str, parameters_dict.values())).replace('[', '').replace(']', '').replace(' ', '').replace(':', '_').replace(',', '-')
+    plt.savefig(title + '_AUC_' + conf + '.png')
+    plt.show()
+
+def plot_auc_multi_class(y_labels, pred, classes, parameters_dict={}, title='', figsize=(12,12)):
+
+    # Compute ROC curve and ROC area for each class
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    y_labelized = label_binarize(y_labels, classes=classes)
+    pred_labelized = label_binarize(pred, classes=classes)
+
+    n_classes = len(classes)
+
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_labelized[:, i], pred_labelized[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_labelized.ravel(), pred_labelized.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    plt.figure(figsize=figsize)
+    lw = 2
+    plt.plot(fpr["micro"], tpr["micro"],
+          label='micro-average ROC curve (area = {0:0.2f})'
+                ''.format(roc_auc["micro"]),
+          color='deeppink', linestyle=':', linewidth=4)
+
+    plt.plot(fpr["macro"], tpr["macro"],
+          label='macro-average ROC curve (area = {0:0.2f})'
+                ''.format(roc_auc["macro"]),
+          color='navy', linestyle=':', linewidth=4)
+
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+              label='ROC curve of class {0} (area = {1:0.2f})'
+              ''.format(i, roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+    plt.legend(loc="lower right")
+    
     conf = ':'.join(map(str, parameters_dict.values())).replace('[', '').replace(']', '').replace(' ', '').replace(':', '_').replace(',', '-')
     plt.savefig(title + '_AUC_' + conf + '.png')
     plt.show()
@@ -126,7 +193,7 @@ def plot_confusion_matrix(cm, class_names, parameters_dict = {}, title='', figsi
     plt.title(title.replace('_', ' '))
     plt.colorbar()
     tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.xticks(tick_marks, class_names, rotation=90)
     plt.yticks(tick_marks, class_names)
 
     # Compute the labels from the normalized confusion matrix
